82a0488ba1959f33f54f979a40d3247eafafbaaa
pinkyo <pinkyo@outlook.com>
2017-10-07 00:36:15 +0800

    Others#, refit to new version of @pinkyo/validatorjs.

--- test/string.js
+++ test/string.js
@@ -1,6 +1,6 @@
 import test from 'ava';
 import _ from 'lodash';
-import {lenBetween, lenGT, lenLT} from '@pinkyo/validatorjs/lib/string';
+import {lenBetween, lenGT, lenLT} from '@pinkyo/validatorjs/lib/tools/string';
 
 const name = "test.name";
 const stringValue = 'test';
@@ -8,15 +8,5 @@ const stringField = {name, value: stringValue};
 
 test('length is between 4 and 4', t => {
   const result = lenBetween(4, 4)(stringField);
-  t.falsy(result);
-});
-
-test('length is greater than 3', t => {
-  const result = lenGT(3)(stringField);
-  t.falsy(result);
-});
-
-test('length is less than 5', t => {
-  const result = lenLT(5)(stringField);
   t.falsy(result);
 });
